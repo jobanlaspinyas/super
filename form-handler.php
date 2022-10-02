8c7535c48717749b0a68a1aca9aadbbeb08cf93b
jobanlaspinyas <114849328+jobanlaspinyas@users.noreply.github.com>
2022-10-02 15:11:04 +0800

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'parezco.000webhostapp.com';
+$email_from = 'https://jobanlaspinyas.github.io/super/';
 
 $email_subject = 'New Form Submission';
 
